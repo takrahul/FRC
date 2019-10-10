3eea4eef19e92720f8c9302bc1bd6c967680a075
lugg3 <lukasmheimann@gmail.com>
2019-10-10 10:54:41 +0200

    Setting up Visual Studio Project

---
+++ Software/UniFCR/UniFCR_GUI/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace UniFCR_GUI {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.titlePanel = new System.Windows.Forms.Panel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuPanel = new System.Windows.Forms.Panel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.optionsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.trainButton = new System.Windows.Forms.Button();
+            this.buttonPanel = new System.Windows.Forms.Panel();
+            this.optionsPanel = new System.Windows.Forms.Panel();
+            this.backButton = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.titlePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuPanel.SuspendLayout();
+            this.buttonPanel.SuspendLayout();
+            this.optionsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.menuPanel);
+            this.mainPanel.Controls.Add(this.titlePanel);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(800, 500);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // titlePanel
+            // 
+            this.titlePanel.BackColor = System.Drawing.Color.White;
+            this.titlePanel.Controls.Add(this.pictureBox1);
+            this.titlePanel.Controls.Add(this.titleLabel);
+            this.titlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlePanel.Location = new System.Drawing.Point(0, 0);
+            this.titlePanel.Name = "titlePanel";
+            this.titlePanel.Size = new System.Drawing.Size(800, 157);
+            this.titlePanel.TabIndex = 0;
+            this.titlePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.titlePanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.titlePanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(409, 20);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(374, 115);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "UniFCR";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 115);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(49)))), ((int)(((byte)(128)))));
+            this.menuPanel.Controls.Add(this.buttonPanel);
+            this.menuPanel.Controls.Add(this.optionsPanel);
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuPanel.ForeColor = System.Drawing.Color.DarkGray;
+            this.menuPanel.Location = new System.Drawing.Point(0, 155);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(800, 345);
+            this.menuPanel.TabIndex = 1;
+            this.menuPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.menuPanel_Paint);
+            // 
+            // startButton
+            // 
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.DarkGray;
+            this.startButton.Location = new System.Drawing.Point(3, 3);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(170, 48);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // optionsButton
+            // 
+            this.optionsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.optionsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optionsButton.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsButton.ForeColor = System.Drawing.Color.DarkGray;
+            this.optionsButton.Location = new System.Drawing.Point(282, 3);
+            this.optionsButton.Name = "optionsButton";
+            this.optionsButton.Size = new System.Drawing.Size(170, 48);
+            this.optionsButton.TabIndex = 1;
+            this.optionsButton.Text = "OPTIONS";
+            this.optionsButton.UseVisualStyleBackColor = true;
+            this.optionsButton.Click += new System.EventHandler(this.optionsButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.Color.DarkGray;
+            this.exitButton.Location = new System.Drawing.Point(279, 99);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(170, 48);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // trainButton
+            // 
+            this.trainButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trainButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.trainButton.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trainButton.ForeColor = System.Drawing.Color.DarkGray;
+            this.trainButton.Location = new System.Drawing.Point(3, 102);
+            this.trainButton.Name = "trainButton";
+            this.trainButton.Size = new System.Drawing.Size(170, 48);
+            this.trainButton.TabIndex = 3;
+            this.trainButton.Text = "TRAIN";
+            this.trainButton.UseVisualStyleBackColor = true;
+            this.trainButton.Click += new System.EventHandler(this.trainButton_Click);
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonPanel.Controls.Add(this.startButton);
+            this.buttonPanel.Controls.Add(this.trainButton);
+            this.buttonPanel.Controls.Add(this.optionsButton);
+            this.buttonPanel.Controls.Add(this.exitButton);
+            this.buttonPanel.Location = new System.Drawing.Point(173, 97);
+            this.buttonPanel.MaximumSize = new System.Drawing.Size(452, 150);
+            this.buttonPanel.MinimumSize = new System.Drawing.Size(452, 150);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(452, 150);
+            this.buttonPanel.TabIndex = 4;
+            this.buttonPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.buttonPanel_Paint);
+            // 
+            // optionsPanel
+            // 
+            this.optionsPanel.Controls.Add(this.backButton);
+            this.optionsPanel.Location = new System.Drawing.Point(130, 48);
+            this.optionsPanel.Name = "optionsPanel";
+            this.optionsPanel.Size = new System.Drawing.Size(528, 232);
+            this.optionsPanel.TabIndex = 5;
+            // 
+            // backButton
+            // 
+            this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backButton.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backButton.ForeColor = System.Drawing.Color.DarkGray;
+            this.backButton.Location = new System.Drawing.Point(174, 84);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(170, 48);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "BACK";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.titlePanel.ResumeLayout(false);
+            this.titlePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuPanel.ResumeLayout(false);
+            this.buttonPanel.ResumeLayout(false);
+            this.optionsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel menuPanel;
+        private System.Windows.Forms.Panel titlePanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button trainButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button optionsButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Panel buttonPanel;
+        private System.Windows.Forms.Panel optionsPanel;
+        private System.Windows.Forms.Button backButton;
+    }
+}
+
---
+++ Software/UniFCR/UniFCR_GUI/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniFCR_GUI {
+    public partial class Form1 : Form {
+        private bool mouseDown;
+        private Point lastLocation;
+        public Form1()
+        {
+            InitializeComponent();
+            optionsPanel.Visible = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void trainButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionsButton_Click(object sender, EventArgs e)
+        {
+            buttonPanel.Visible = false;
+            optionsPanel.Visible = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+        }
+
+        private void menuPanel_Paint(object sender, PaintEventArgs e)
+        {
+            //Center the Buttons 
+            int buttonPanelX = (this.Size.Width / 2) - (buttonPanel.Size.Width / 2);
+            int buttonPanelY = (this.Size.Height / 2) - (buttonPanel.Size.Height);
+            buttonPanel.Location = new Point(buttonPanelX, buttonPanelY);
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            optionsPanel.Visible = false;
+            buttonPanel.Visible = true;
+        }
+    }
+}

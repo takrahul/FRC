42368cece143817c81af5cb0548a20a6b96b2ce1
lugg3 <lukasmheimann@gmail.com>
2019-10-11 09:20:10 +0200

    GUI for Attendance Mode added...

--- Software/UniFCR/UniFCR_GUI/Program.cs
+++ Software/UniFCR/UniFCR_GUI/Program.cs
@@ -14,7 +14,7 @@ namespace UniFCR_GUI {
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MenuScreen());
         }
     }
 }
